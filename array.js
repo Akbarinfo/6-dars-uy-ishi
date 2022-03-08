46f8e83badabf280f8e64dfced2ec5f5275cd51a
akbarinfo <girtbezbet@gmail.com>
2022-03-08 09:09:08 +0500

    update

---
+++ array.js
@@ -0,0 +1,93 @@
+// Array 1
+
+// let n = 10;
+// let barray = []
+// for(let i = 0; i < n *2; i++) {
+//   if(i % 2 != 0){
+//     barray.push(i);
+//   }
+// }
+// console.log(barray);
+
+
+// Array 2
+
+// let n = 10;
+// let abox = 1;
+// let barray = [];
+
+// for(let i = 1; i <= n; i++) {
+//   abox += i*i;
+//   barray.push(abox);
+// }
+// console.log(barray);
+
+
+// Array 3
+
+
+// Array 4
+
+
+// Array 5
+
+// let n = 10;
+// let ason = 1;
+// let bson = 1;
+// let array = [];
+// let result;
+
+// for(let i = 1; i <= n; i++) {
+//   result = ason + bson;
+//   ason = bson;
+//   bson = result;
+//   array.push(result);
+// }
+// console.log(array)
+
+// Array 6
+
+
+// Array 7
+
+// let n = [2, 6, 3, 5, 8, 31, 45];
+// console.log(n.reverse());
+
+
+// Array 8
+
+let n = [11, 1, 13, 4, 5, 10, 7, 8, 9, 57, 21, 17];
+let count = 0;
+
+for(let i = 1; i < n.length; i++){
+  let acount = 0;
+  if(n[i] % 2 != 0) {
+    console.log(n[i]);
+    count++;
+  }
+}
+console.log(`${count} ta toq son`);
+
+
+// Array 9
+
+// let n = [11, 1, 13, 4, 5, 10, 7, 8, 9, 57, 21, 17];
+// let count = 0;
+
+// for(let i = 1; i <= n.length; i++){
+//   let acount = 0;
+//   if(n[i] % 2 == 0) {
+//     console.log(n[i]);
+//     count++;
+//   }
+// }
+// console.log(`${count} ta juft son`);
+
+// Array 10
+// Array 11
+// Array 12
+// Array 13
+// Array 14
+// Array 15
+// Array 16
+// Array 17
